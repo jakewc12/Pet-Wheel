7ad8b3303352cfaf2e86a822b323814e9d9002be
Jake Wu-Chen <97861475+jakewc12@users.noreply.github.com>
2023-02-23 20:10:24 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,34 @@
-import xdrlib
 from kivy.app import App
-from kivy.metrics import dp
-from kivy.uix.togglebutton import ToggleButton
-from kivy.properties import StringProperty,BooleanProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.stacklayout import StackLayout
-
-class WidgetsExample(GridLayout):
-    my_text = StringProperty("0")
-    count = 0
-    count_enable = BooleanProperty(False)
-    def on_button_click(self):
-        if self.count_enable:
-            self.count+=1
-            self.my_text = str(self.count)
-    def on_toggle_button_state(self, widget):
-        if widget.state == "normal":
-            # OFF
-            widget.text = "OFF"
-            self.count_enable = False
-        else:
-            # ON
-            widget.text = "ON"
-            self.count_enable = True
-
-class StackLayoutExample(StackLayout):
-    pass
-   #def __init__(self, **kwargs):
-    #   super().__init__(**kwargs)
-    #   l = Label(text="body",font_size='20sp',size_hint=(None,None),size=(dp(500),dp(500)))
-    #   self.add_widget(l)
-       #for i in range(0,10):
-       #     b = Label(text=str(i+1), size_hint=(None,None),size=(dp(100),(dp(100))))
-       #     self.add_widget(b)
-
-class MainWidget(Widget):
+from kivy.uix.image import Image
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.textinput import TextInput
+import time
+class BoxLayoutExample(FloatLayout):
+    startHealth = 100
+    startSeconds = 1677041749.9353511
+    #instead of using this random number for start seconds
+    #we should use the time the game was launched
+    changedTime = time.time() - startSeconds
+    changedValue = changedTime/1800
+    #decreases 1 every half day
+    changedFinal = 100 -int(changedValue)
+    status = "Status: "
+    if changedFinal >= 75:
+        status += " Healthy"
+    elif changedFinal >= 50:
+        status += " Hungry"
+    elif changedFinal > 0:
+        status += " Needs help"
+    else:
+        status += " Dead"
+    
+class MainWiget(Widget):
     pass
 
-class TheLabApp(App):
+class PetWheel(App):
     pass
-#class BoxLayoutExample(BoxLayout):
- #   pass
-""""
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-        b1 = Button(text="A")
-        b2 = Button(text="A")
-        self.add_widget(b1)
-        self.add_widget(b2)
-"""
 
-TheLabApp().run()
+PetWheel().run()
